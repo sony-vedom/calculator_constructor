20af6f39f7dfbe88536eb134dbb85e73b1a87f71
sony-vedom <minerva.lankmiller@gmail.com>
2023-03-12 21:31:51 +0300

    Calculator was added

--- src/redux/calculatorData.js
+++ src/redux/calculatorData.js
@@ -1,7 +1,17 @@
+const PLUS = "PLUS"
+const MINUS = "MINUS"
+const DEVISION = "DEVISION"
+
+
 const initialState = {
     numbers: [",", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     operators: ["/", "х", "-", "+"],
+    output: 0,
 }
+
+const plusAction = (number) => ({
+    type: PLUS
+})
 
 const calculatorData = (state = initialState) => state;
 
